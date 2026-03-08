197adb365d2ba437baf94d1b7d281aa2c424d1ca
abrandonment <brandonmichael109@gmail.com>
2026-03-08 17:48:01 -0400

    Fully Done Menus
    -Added a VHS glitch effect to main menu
    -Added SceneTransition
    -Pause menu can now take you back to main menu
    -Added a sound effect for the transition

--- HwG Prototype/Assets/General scripts/SceneLoader.cs
+++ HwG Prototype/Assets/General scripts/SceneLoader.cs
@@ -1,11 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     public void restartScene()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public Animator transition;
+    public float transitionTime = 2f;
+    public AudioSource music;
+    public AudioSource transitionSFX;
+    public string transitionTrigger;
+
+    //Loads Next Level in build
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    public void ReloadLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Stop music if music is set
+        if (music != null)
+        {
+            music.Stop();
+        }
+        //Play Transition Animation
+        transition.SetTrigger(transitionTrigger);
+        //Play Transition Audio if set
+        if (transitionSFX != null)
+        {
+            transitionSFX.Play();
+        }
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
     }
 }
