76097a1e849e3db42f48e1232c6e60fd647ce55b
ValentinoM132 <Valentinomartinez00@gmail.com>
2026-03-09 00:48:43 -0400

    FINAL CUTSCENE

--- HwG Prototype/Assets/General scripts/switchactivate.cs
+++ HwG Prototype/Assets/General scripts/switchactivate.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class switchactivate : MonoBehaviour
 {
     public Animator animator;
+    public PlayableDirector Timeline;
 
     public void flipSwitch()
     {
         animator.SetTrigger("Flip");
+        RenderSettings.fog = false;
+        Timeline.Play();
+        StartCoroutine(goMainMenu());
+    }
+
+    IEnumerator goMainMenu()
+    {
+        yield return new WaitForSeconds(6.7f);
+        SceneLoader sceneLoader = FindObjectOfType<SceneLoader>();
+        sceneLoader.LoadMainMenu();
     }
 }
